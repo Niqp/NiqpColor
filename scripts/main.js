ce96c7ea5377ef8aac42d3cfa236808635591e0e
IupatovNikita <hukull3@gmail.com>
2022-09-04 16:00:26 +0300

    Rewrote the script to use json video database

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,54 @@
-let videoTemplates = document.querySelectorAll(".videoTemplate");
-let popupWindow = document.querySelector(".videoPopup");
-let popupEmbed = document.querySelector(".popupEmbed")
+const POPUP_WINDOW = document.querySelector(".videoPopup");
+const POPUP_EMBED = document.querySelector(".popupEmbed");
+const LOADING_PORTFOLIO = document.querySelector(".loadingPortfolio");
+const PAGE_LANGUAGE = document.documentElement.lang;
+const VIDEO_DATA_LOCATION = "data/videos.json";
 
-console.log(videoTemplates);
 
-popupWindow.addEventListener('click', function () {
-    popupWindow.classList.remove('videoPopup--open');
-    popupWindow.classList.add('videoPopup--closed');
-    popupEmbed.src = "";
-});
-
-for (let index = 0; index < videoTemplates.length; index++) {
-    videoTemplates[index].addEventListener('click', function () {
-        popupWindow.classList.remove('videoPopup--closed');
-        popupWindow.classList.add('videoPopup--open');
-        popupEmbed.src = videoTemplates[index].dataset.videoYtId + "?autoplay=1&rel=0";
-      });
-    
+const PopupAddButtonListener = () => {
+    POPUP_WINDOW.addEventListener('click', function () {
+        POPUP_WINDOW.classList.remove('videoPopup--open');
+        POPUP_WINDOW.classList.add('videoPopup--closed');
+        POPUP_EMBED.src = "";
+    });
 }
 
+const videoTemplate = (element) => (   
+    `<div class="videoTemplate" id="${element.id}" data-video-yt-id="${element.link}">
+    <div class="videoName">
+        <div class="videoFlex">
+            <h3>${PAGE_LANGUAGE==="ru"?element.name:element.nameEN}</h3>
+            <img src="img/play2.png" alt="Воспроизвести">
+            <p>${PAGE_LANGUAGE==="ru"?element.description:element.descriptionEN}</p>
+        </div>
+    </div>
+    <img class="videoBackground" src="img/videoThumbnails/${element.id}.jpg" alt="${element.name}">
+    </div>`
+    );
+    
+const GenerateVideoBlocks = (videos) => {
+    let videoContainer = document.querySelector(".portfolio");
+    LOADING_PORTFOLIO.classList.remove('loadingPortfolio');
+    LOADING_PORTFOLIO.classList.add('videoTemplate--closed');
+    for (let index=0; index < videos.length; index++) {
+        let videoGeneratedTemplate = document.createElement('div');
+        videoGeneratedTemplate.innerHTML = videoTemplate(videos[index]);
+        let videoTemplateClass = videoGeneratedTemplate.querySelector(".videoTemplate");
+        videoTemplateClass.addEventListener('click', () => {
+            POPUP_WINDOW.classList.remove('videoPopup--closed');
+            POPUP_WINDOW.classList.add('videoPopup--open');
+            POPUP_EMBED.src = videos[index].link + "?autoplay=1&rel=0";
+        })
+        videoContainer.append(videoGeneratedTemplate.firstChild);
+    }
+}
+
+fetch(VIDEO_DATA_LOCATION)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let videos = data.VideosList;
+        PopupAddButtonListener();
+        GenerateVideoBlocks(videos);
+    })
